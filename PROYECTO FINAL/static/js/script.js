82169999bec90dc411d02ad0958f1a1fb4ba9e7b
cristianrm24 <soliscris72@gmail.com>
2024-06-03 13:06:46 -0600

    Barras laterales admin

--- PROYECTO FINAL/static/js/script.js
+++ PROYECTO FINAL/static/js/script.js
@@ -11,3 +11,18 @@ cerrar.addEventListener("click", () => {
     nav.classList.remove('visible');
 })
 
+function mostrarFormularioSubida(proyectoId) {
+    var form = document.getElementById('form_pdf');
+    form.action = "{{ url_for('admin_app.subir_pdf', proyecto_id='') }}" + proyectoId;
+    document.getElementById('modal-subida').style.display = 'flex';
+}
+
+function subirPDF() {
+    var form = document.getElementById('form_pdf');
+    form.submit();
+}
+
+function cerrarModal() {
+    document.getElementById('modal-subida').style.display = 'none';
+}
+
